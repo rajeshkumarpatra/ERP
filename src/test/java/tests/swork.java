8cbc270f4700530aa935935077ae6c303eb6f786
jannathparveen <jannathparveen@LAPTOP-B8B5PKMH>
2019-11-08 11:11:36 +0530

    hello

---
+++ src/test/java/tests/swork.java
@@ -0,0 +1,14 @@
+package tests;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import generics.BaseTest;
+
+public class swork extends BaseTest {
+@Test
+public static void Demo1()	
+{
+Reporter.log("hello",true);
+}
+}
