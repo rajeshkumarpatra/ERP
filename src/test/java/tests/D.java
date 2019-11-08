332a8e64223846b632223f54e759dd88215898b6
Admin <mariaphillips2323@gmail.com>
2019-11-08 08:59:36 +0530

    add class

---
+++ src/test/java/tests/D.java
@@ -0,0 +1,13 @@
+package tests;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class D 
+{
+	@Test
+    public static void work()
+    {
+    	Reporter.log("HI dude", true);
+    }
+}
