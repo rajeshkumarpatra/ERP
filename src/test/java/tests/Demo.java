595818bf7e23479d4ba7277758d5fc0d9ef58ae6
310056495 <310056495@YY111655.code1.emi.philips.com>
2019-11-08 09:18:40 +0530

    NewClass_Kavya

---
+++ src/test/java/tests/Demo.java
@@ -0,0 +1,14 @@
+package tests;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo 
+{
+	@Test
+    public static void main()
+    {
+    	Reporter.log("HI",true);
+    }
+    
+}
